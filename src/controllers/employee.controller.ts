d6710597190b19827b87c8cfe3d2afecd040ba2d
PrannavP <ramsarilekhi@gmail.com>
2025-11-14 22:10:03 +0545

    Update employee validation schema & endpoint complete.

--- src/controllers/employee.controller.ts
+++ src/controllers/employee.controller.ts
@@ -67,3 +67,57 @@ export const onBoardNewEmployee = async (req: Request, res: Response) => {
         });
     }
 };
+
+// controller for admin to edit employee details
+export const editEmployeeDetails = async (req: Request, res: Response) => {
+    try{
+        const data = req.body;
+
+        // Check if employee exists
+        const employeeExists = await prisma.user.findUnique({
+            where: { id: data.id }
+        });
+
+        // If existing then throw error
+        if(!employeeExists) throw new Error("Employee does not exists.");
+
+        // If exists then check if the password is updated or not
+        let passwordToUse: string;
+        const isSamePassword = await bcrypt.compare(data.password, employeeExists.password);
+        if (isSamePassword) {
+            passwordToUse = employeeExists.password;
+        } else {
+            passwordToUse = await bcrypt.hash(data.password, 10);
+        }
+
+        const updatedEmployee = await prisma.user.update({
+            where: {
+                id: data.id
+            },
+            data: {
+                full_name: data.full_name,
+                email: data.email,
+                password: passwordToUse,
+                address: data.address,
+                contact_number: data.contact_number,
+                joined_date: data.joined_date,
+                gender: data.gender,
+                type: data.type, // employee type
+                date_of_birth: data.date_of_birth,
+                status: data.status,
+                Branch: data.Branch,
+                Department: data.Department,
+                Designation: data.Designation,
+                user_type: data.user_type
+            }
+        });
+
+        res.status(200).json({ message: "Employee Updated Successfully.", updatedEmployee });
+    }catch(err){
+        console.error("Error updating employee", err);
+        res.status(500).json({
+            message: "Error updating employee.",
+            errror: err instanceof Error ? err.message : String(err)
+        });
+    }
+};
--- src/routes/employee.routes.ts
+++ src/routes/employee.routes.ts
@@ -2,17 +2,27 @@
 // ADMIN ROUTES ASWELL AS EMPLOYEE ROUTES
 
 // Validation schemas
-import { onBoardNewEmployeeSchema } from "../validation/employee.schema";
+import {
+    onBoardNewEmployeeSchema,
+    updateEmployeeSchema
+} from "../validation/employee.schema";
+
 // Zod validation middleware
 import { validate } from "../middlewares/validate";
 
 import { Router } from "express";
 import { authMiddleware, authorize } from "../middlewares/auth.middleware";
-import { onBoardNewEmployee } from "../controllers/employee.controller";
+import {
+    onBoardNewEmployee,
+    editEmployeeDetails
+} from "../controllers/employee.controller";
 
 const router = Router();
 
 // Protected route aswell as only admin access routes
 router.post('/onboardNewEmployee', validate(onBoardNewEmployeeSchema), authMiddleware, authorize(["admin"]), onBoardNewEmployee);
+
+// Use the same validation for employee create
+router.post('/updateEmployee', validate(updateEmployeeSchema), authMiddleware, authorize(["admin"]), editEmployeeDetails);
 
 export default router;
